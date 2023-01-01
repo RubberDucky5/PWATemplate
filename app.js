8fd31782a2e09c1ed2df401028dceb8a3dc190dd
RubberDuck55 <woodjb17@gmail.com>
2023-01-01 16:10:27 -0500

    Added "Tap!" for mobile clients

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ function registerServiceWorker () {
     if (!('serviceWorker' in navigator)) return;
     navigator.serviceWorker.register('sw.js');
 }
-
 registerServiceWorker();
+
+// Test For mobile
+if(/iPhone|Android/i.test(navigator.userAgent)) {
+    document.getElementById("click").innerHTML = "Tap!";
+}
 
 let clicks = 0;
 
